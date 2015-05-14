7a812a80604373fb25bcbf4dc6d3dc19d03bbe9f
sebastianff <sebastianffarago@gmail.com>
2015-05-14 08:26:34 +0200

    Functioning version

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x,y,z) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = x;
+    this.y = y;
+    this.speed = z;
 }
 
 // Update the enemy's position, required method for game
@@ -14,6 +17,10 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x = this.x+this.speed*dt;
+    
+    if(this.x>505)
+        {this.x=1}
 }
 
 // Draw the enemy on the screen, required method for game
@@ -25,15 +32,55 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+var Player = function(x,y) {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
 
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/char-boy.png';
+    this.x = x;
+    this.y = y;
+    
+}
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
 
-
+Player.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+    
+    
+}
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
+Player.prototype.handleInput = function(stroke)
+{   
+    switch(stroke)
+    {
+        case  "left":
+        if (this.x - fromWall>0){this.x = this.x-playerRange};
+        break;
+        case "right":
+         if (this.x + fromWall<400){this.x = this.x+playerRange};
+        break;
+        case "down":
+        if (this.y + fromWall<490){this.y = this.y+playerRange};
+        break;
+        case "up":
+        if (this.y - fromWall>0){this.y = this.y-playerRange};
+        break;
+
+    }
+    
+
+}
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
@@ -45,3 +92,17 @@ document.addEventListener('keyup', function(e) {
     player.handleInput(allowedKeys[e.keyCode]);
 });
 
+var enemySpeed = 30;
+allEnemies = [];
+playerLocation =[];
+for (i=1;i<4;i++)
+{allEnemies.push(new Enemy(3,70*i,randomNumber()*enemySpeed))};
+
+var player = new Player(80,300);
+
+function randomNumber() {var number = Math.floor((Math.random() * 10) + 1);return number;}
+console.log(randomNumber());
+
+var distance = 60;
+var playerRange = 100;
+var fromWall = 5;
--- js/engine.js
+++ js/engine.js
@@ -80,7 +80,10 @@ var Engine = (function(global) {
      */
     function update(dt) {
         updateEntities(dt);
-        // checkCollisions();
+        for (item in allEnemies)
+                if(player.x - allEnemies[item].x <distance&&allEnemies[item].x - player.x<distance&&player.y - allEnemies[item].y <distance&&allEnemies[item].y - player.x<distance )
+           {reset()}
+       
     }
 
     /* This is called by the update function  and loops through all of the
@@ -160,7 +163,7 @@ var Engine = (function(global) {
      * those sorts of things. It's only called once by the init() method.
      */
     function reset() {
-        // noop
+        player.x = 200;player.y =400;
     }
 
     /* Go ahead and load all of the images we know we're going to need to
